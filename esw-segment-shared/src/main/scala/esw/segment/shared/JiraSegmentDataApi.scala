35d5ade70365e6feafac7764becd440d7924742e
abrighto <Allan.Brighton@t-online.de>
2020-12-13 21:07:13 +0100

    Started JIRA based API

--- esw-segment-shared/src/main/scala/esw/segment/shared/JiraSegmentDataApi.scala
+++ esw-segment-shared/src/main/scala/esw/segment/shared/JiraSegmentDataApi.scala
@@ -9,6 +9,6 @@ trait JiraSegmentDataApi {
    * Returns the segment positions as defined in the JIRA issues
    * (Missing segments are also included in the returned list).
    */
-  def currentPositions(): Future[List[SegmentToM1Pos]]
+//  def currentPositions(): Future[List[SegmentToM1Pos]]
 
 }
