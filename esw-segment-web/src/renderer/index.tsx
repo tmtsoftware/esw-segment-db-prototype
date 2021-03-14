5d725dca9266e6425649c1a26bd92e3d3ddb1b31
abrighto <Allan.Brighton@t-online.de>
2021-03-14 19:42:25 +0100

    Changed to be an Electron based app.

--- esw-segment-web/src/renderer/index.tsx
+++ esw-segment-web/src/renderer/index.tsx
@@ -3,7 +3,10 @@ import {render} from 'react-dom'
 import './index.css'
 import App from './App'
 import {AuthContextProvider} from "@tmtsoftware/esw-ts";
-import {AppConfig} from "./config/AppConfig";
+import {setAppConfigPath} from '@tmtsoftware/esw-ts'
+import {AppConfig} from "./AppConfig";
+
+setAppConfigPath('dist/AppConfig.js')
 
 render(
   <AuthContextProvider config={AppConfig}>
